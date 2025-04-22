687f493a4185fc08bf6183adbd04e0604d0f4221
Cássio Marcião <marciaocassio@gmail.com>
2025-04-22 20:30:48 -0300

    chore: Config tailwindcss for vite

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,28 +1,27 @@
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
-	content: [
-		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
-		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
-	],
-	theme: {
-		extend: {
-			colors: {
-				primary: '#FFDE07'
-			},
-			backgroundColor: {
-				'gray-900': '#151515'
-			},
-			transitionProperty: {
-				'hover': '.2s all'
-			},
-			maxWidth: {
-				'screen-sm': '380px',
-				'screen-md': '520px'
-			},
-		},
-	},
-	plugins: [],
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}"
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primary: '#FFDE07'
+            },
+            backgroundColor: {
+                'gray-900': '#151515'
+            },
+            transitionProperty: {
+                'hover': '.2s all'
+            },
+            maxWidth: {
+                'screen-sm': '380px',
+                'screen-md': '520px'
+            },
+        },
+    },
+    plugins: [],
 };
 export default config;
